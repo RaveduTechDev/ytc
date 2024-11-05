adadf4581269fc349caf8361636ccd2334b1630c
Ariel Gema Wardana <112913828+ArielGwd@users.noreply.github.com>
2024-11-05 16:45:23 +0700

    update: background & content

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
+const header = document.querySelector(".header_section");
+document.addEventListener("scroll", () => {
+  header.classList.toggle("fixed-top", window.scrollY > 0);
+  header.classList.toggle("sticky_nav", window.scrollY > 0);
+});
+
 // get current year
 (function () {
-    var year = new Date().getFullYear();
-    document.querySelector("#currentYear").innerHTML = year;
+  var year = new Date().getFullYear();
+  document.querySelector("#currentYear").innerHTML = year;
 })();
